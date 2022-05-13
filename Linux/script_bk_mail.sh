ee0c0e69c34cbf67664f083a63855359a9713c54
Gabriel Manrique <gabimanrique14@gmail.com>
2022-05-13 16:51:53 -0300

    Translate to English bash bk script

--- Linux/script_bk_mail.sh
+++ Linux/script_bk_mail.sh
@@ -1,46 +1,46 @@
 #!/bin/bash
-RUTA_SCRIPT=`dirname ${BASH_SOURCE[0]}`
-FECHA_Y_HORA=`date "+%d-%m-%y_%H-%M-%S"`
-FECHA=`date "+%d-%m-%y"`
-NOMBRE_ARCHIVO="Thunder_$FECHA.tgz"
+PATH_SCRIPT=`dirname ${BASH_SOURCE[0]}`
+DATE_Y_TIME=`date "+%d-%m-%y_%H-%M-%S"`
+DATE=`date "+%d-%m-%y"`
+FILE_NAME="Thunder_$DATE.tgz"
 LINES=0
-CARPETA_DESTINO="$RUTA_SCRIPT/Bk_email_automatico"
-# Nota: la carpeta a respaldar puede ser otra, pero si no es relativa al script, pon su ruta completa
-CARPETA_RESPALDAR="/home/gabriel/.thunderbird"
-# Creamos el directorio para los respaldos por si no existe
-crear_carpeta()
+DEST_FOLDER="$PATH_SCRIPT/Bk_email_automatico"
+# Note: You can change the folder to bk, if folder isnt in the path script, write absolute path
+SOURCE_FOLDER="/home/user/.thunderbird"
+# make directory 
+make_dir()
 {
-  mkdir -p "$CARPETA_DESTINO"
+  mkdir -p "$DEST_FOLDER"
 }
 
-#Creamos un condicional para verificar si existe y no duplicar, de manera indirecta evito el uso excesivo de espacio
-#Ademas implemento el uso de logs para verificar en un archivo los fallos del programa
-comprimir_archivo()
+# Check if file exists 
+# Add log output to watch result of script
+compress_files()
 {
-if [ -f /home/gabriel/Documents/Bk_email_automatico/$NOMBRE_ARCHIVO ];then
-  echo "El archivo no se creo,porque ya existe un archivo con el mismo nombre-->  $FECHA_Y_HORA" >> $CARPETA_DESTINO/log.txt;
-  echo "El proceso de backup finalizo con errores. Ver Logs"
+if [ -f /home/user/Documents/Bk_email_automatico/$FILE_NAME ];then
+  echo "Dont create the file because it already exists-->  $DATE_Y_TIME" >> $DEST_FOLDER/log.txt;
+  echo "Backup process finished with errors. See Logs"
 else
-#Comprimo con TAR  c es para crear, la v para que sea verboso o imprima mensajes de debug, la z para que los archivos sean comprimidos con gzip 
-#y f para que opere sobre el archivo   	
-  tar cfvz "$CARPETA_DESTINO/$NOMBRE_ARCHIVO" --absolute-names "$CARPETA_RESPALDAR";
-  echo "El proceso de backup finalizo exitosamente -->  $FECHA_Y_HORA" >> $CARPETA_DESTINO/log.txt
+# Package file with tar and add options  c for make, v is verbose mode, z to compress with gzip 
+# And f to operate over the file   	
+  tar cfvz "$DEST_FOLDER/$FILE_NAME" --absolute-names "$DEST_FOLDER";
+  echo "Backup process finished susccessfully -->  $DATE_Y_TIME" >> $DEST_FOLDER/log.txt
 fi
 }
-#Le asigno a la variable LINES el resultado de la busqueda, ademas con wc cuenta las palabras y con -l las lineas
-LINES=$(find /home/gabriel/Documents/Bk_email_automatico/Thunder* -mtime +14 | wc -l)
+#Assign into LINES the rearch result, also with "wc -l" count the lines to compare later
+LINES=$(find /home/user/Documents/Bk_email_automatico/Thunder* -mtime +14 | wc -l)
 
-#En base a lo anterior ejecuto la condicion para tener un control de la eliminacion de bk antiguos y registro el resultado
-eliminar_antiguos()
+# Do the comparison and delete old files
+delete_oldfiles()
 {
 if [ "$LINES" -eq 0  ]; then
-  echo "No se encontraron archivos en la busqueda. No se borraron backups antiguos -->  $FECHA_Y_HORA" >> $CARPETA_DESTINO/log.txt;
+  echo "Dont find files in the search . Dont delete olds backups -->  $DATE_Y_TIME" >> $DEST_FOLDER/log.txt;
 else
-  find /home/gabriel/Documents/Bk_email_automatico/Thunder* -mtime +14 -delete;
-  echo "Se encontraron archivos en la busqueda. Se borraron backups antiguos -->  $FECHA_Y_HORA" >> $CARPETA_DESTINO/log.txt;
+  find /home/user/Documents/Bk_email_automatico/Thunder* -mtime +14 -delete;
+  echo "Find files in the search. Olds files were delete -->  $DATE_Y_TIME" >> $DEST_FOLDER/log.txt;
 fi
 }
-#cat $CARPETA_DESTINO/log.txt
-crear_carpeta
-comprimir_archivo
-eliminar_antiguos
+
+make_dir
+compress_files
+delete_oldfiles
